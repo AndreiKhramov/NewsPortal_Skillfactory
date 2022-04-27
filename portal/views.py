da25fdaffcaaec1ca8cd649df9ea21f7376652d8
AndreiKhramov <95291811+AndreiKhramov@users.noreply.github.com>
2022-04-27 16:10:12 +0300

    Add files via upload

--- portal/views.py
+++ portal/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from datetime import datetime
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+
+class NewsList(ListView):
+    model = Post
+    ordering = 'add_time'
+    template_name = 'news.html'
+    context_object_name = 'news'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['next_post'] = None
+        return context
+
+
+class NewsDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
