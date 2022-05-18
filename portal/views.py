50346870997c00e10bdd3c23df440e8e16d1bdc2
AndreiKhramov <95291811+AndreiKhramov@users.noreply.github.com>
2022-05-18 10:59:14 +0300

    Add files via upload

--- portal/views.py
+++ portal/views.py
@@ -2,17 +2,21 @@ from datetime import datetime
 from django.views.generic import ListView, DetailView
 from .models import Post
 
+from .filters import PostFilter
+
 
 class NewsList(ListView):
     model = Post
     ordering = 'add_time'
     template_name = 'news.html'
     context_object_name = 'news'
+    paginate_by = 1
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['time_now'] = datetime.utcnow()
-        context['next_post'] = None
+        context['filter'] = PostFilter(self.request.GET, queryset=self.get_queryset())
+        # context['time_now'] = datetime.utcnow()
+        # context['next_post'] = None
         return context
 
 
